d3ff81adbacd290d69d4b5dfc49632437e169205
LoMaply <linuslee71@gmail.com>
2024-01-11 14:24:45 +0800

    Update migration

--- backend/db/migrate/20231211064137_create_discussions.rb
+++ backend/db/migrate/20231211064137_create_discussions.rb
@@ -3,6 +3,7 @@ class CreateDiscussions < ActiveRecord::Migration[7.1]
     create_table :discussions do |t|
       t.string :title
       t.string :description
+      t.string :category
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
