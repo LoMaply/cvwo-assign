56cdc521b7d814418afdb8f701c03ca7a0d070a1
LoMaply <linuslee71@gmail.com>
2024-01-11 15:13:35 +0800

    Update migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_053333) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_11_064240) do
   create_table "comments", force: :cascade do |t|
     t.string "description"
     t.integer "user_id", null: false
     t.integer "discussion_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "parent_id"
     t.index ["discussion_id"], name: "index_comments_on_discussion_id"
-    t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -39,7 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_053333) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "comments", column: "parent_id"
   add_foreign_key "comments", "discussions"
   add_foreign_key "comments", "users"
   add_foreign_key "discussions", "users"
